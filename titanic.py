fed4ad8bfca675643c372a0787b83069051490d1
wang-hui-shan <52596112+wang-hui-shan@users.noreply.github.com>
2021-04-16 13:26:32 +0800

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -3,65 +3,58 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-#train_data = pd.read_csv('train.csv')
-#train_data.drop(columns=['PassengerId'],inplace=True)
-train_data = pd.concat([pd.read_csv('train.csv'),pd.read_csv('test.csv')])
+#all_data = pd.read_csv('train.csv')
+#all_data.drop(columns=['PassengerId'],inplace=True)
+all_data = pd.concat([pd.read_csv('train.csv'),pd.read_csv('test.csv')])
 # 新增特征
 # 名字前缀
-train_data['prefix'] =  train_data['Name'].apply(lambda x :  x.strip().split(',')[1].strip().split('.')[0])
+all_data['prefix'] =  all_data['Name'].apply(lambda x :  x.strip().split(',')[1].strip().split('.')[0])
 # 家庭成员
-train_data['Fnums'] = train_data['SibSp'] + train_data['Parch'] + 1
+all_data['Fnums'] = all_data['SibSp'] + all_data['Parch'] + 1
 # Fare=0替换为np.nan
-train_data['Fare'].replace(0.0,np.nan,inplace=True)
+all_data['Fare'].replace(0.0,np.nan,inplace=True)
 
 #1.Cabin列缺失值过多，暂时清除,Name,Ticket包含文本数据，暂时认为和姓名,ticket无关
-train_data.drop(columns=['Cabin', 'Name', 'Ticket'],inplace=True)
+all_data.drop(columns=['Cabin', 'Name', 'Ticket'],inplace=True)
 
-#查看存在缺失的位置 train_data[train_data.isnull().values==True]
-# Age Nan 暂时用 median 填充，Embarked 用第四种类别 A 替代 Nan
-# values = {'Age': train_data['Age'].median(), 'Embarked': 'C'}
-values = {'Embarked': 'C','Fare':train_data['Fare'].median()}
-train_data.fillna(value=values,inplace=True)
-
-from sklearn.impute import KNNImputer
-imputer = KNNImputer()
-train_data[['Age']] = imputer.fit_transform(train_data[['Age']])
+# Age 用 KNNImputer 填充，Embarked 用出现最多的类别C填充
+values = {'Embarked': 'C','Fare':all_data['Fare'].median(),'Age':all_data['Age'].median()}
+all_data.fillna(value=values,inplace=True)
 
 # 增加特征
 # AgeBin FareBin
-train_data['AgeBin'] = pd.cut(train_data['Age'].astype(int), 5)
-train_data['FareBin'] = pd.cut(train_data['Fare'].astype(int), 25)
-
+all_data['AgeBin'] = pd.cut(all_data['Age'].astype(int), 5)
+all_data['FareBin'] = pd.cut(all_data['Fare'].astype(int), 25)
 
 #数据处理
 #将离散型值转换为数值Categorical
 from sklearn.preprocessing import LabelEncoder
 def labelEncoding():
     le = LabelEncoder()
-    train_data['prefix']  = le.fit_transform(train_data['prefix'])
-    train_data['Embarked']  = le.fit_transform(train_data['Embarked'])
-    train_data['Sex']  = le.fit_transform(train_data['Sex'])
-    train_data['AgeBin'] = le.fit_transform(train_data['AgeBin'])
-    train_data['FareBin'] = le.fit_transform(train_data['FareBin'])
+    all_data['prefix']  = le.fit_transform(all_data['prefix'])
+    all_data['Embarked']  = le.fit_transform(all_data['Embarked'])
+    all_data['Sex']  = le.fit_transform(all_data['Sex'])
+    all_data['AgeBin'] = le.fit_transform(all_data['AgeBin'])
+    all_data['FareBin'] = le.fit_transform(all_data['FareBin'])
 	
 def oneHotEncoding():
-    dum = pd.get_dummies(train_data[['Sex','Embarked','prefix','AgeBin','FareBin']],)
-    data = pd.concat([train_data.drop(columns=['Sex','Embarked','prefix','AgeBin','FareBin']), dum],axis=1)
+    dum = pd.get_dummies(all_data[['Sex','Embarked','prefix','AgeBin','FareBin']],)
+    data = pd.concat([all_data.drop(columns=['Sex','Embarked','prefix','AgeBin','FareBin']), dum],axis=1)
     return data
 
 labelEncoding()
-# train_data = oneHotEncoding()
+# all_data = oneHotEncoding()
 
-X=train_data.drop(columns=['PassengerId', 'Survived'])[:891]
-y=train_data['Survived'][:891]
+X = all_data.drop(columns=['PassengerId', 'Survived'])[:891]
+y = all_data['Survived'][:891]
 
-test_X = train_data.drop(columns=['PassengerId', 'Survived'])[891:]
+test_X = all_data.drop(columns=['PassengerId', 'Survived'])[891:]
 
 # 标准化
 from sklearn.preprocessing import StandardScaler
-scaler = StandardScaler()
-X = scaler.fit_transform(X)
-test_X = scaler.fit_transform(test_X)
+scaler = StandardScaler().fit(X)
+X = scaler.transform(X)
+test_X = scaler.transform(test_X)
 
 # cross_validate交叉验证
 import warnings
@@ -142,41 +135,32 @@ estimators = [
 ]
 
 SC = StackingClassifier(estimators=estimators, final_estimator=SVC)
-cv_results = cross_validate(SC , X, y, cv=10)
+cv_results = cross_validate(SC , X, y, cv=10,return_estimator=True)
 print("SC:",cv_results['test_score'].mean())
 
 VC = VotingClassifier(estimators=estimators)
-cv_results = cross_validate(VC , X, y, cv=10)
+cv_results = cross_validate(VC , X, y, cv=10,return_estimator=True)
 print("VC:",cv_results['test_score'].mean())
-
 # 测试集预测
-clf = StackingClassifier(estimators=estimators, final_estimator=SVC).fit(X,y)
-test_y = clf.predict(test_X)
+best_clf = cv_results['estimator'][cv_results['test_score'].argmax()]
+test_y = best_clf.predict(test_X)
 predict = pd.DataFrame()
-predict['PassengerId'] = train_data['PassengerId'][891:]
-predict['Survived'] = test_y.astype(int)
-predict.to_csv('predic.csv',index=False)
-
-# 测试集预测
-clf = StackingClassifier(estimators=estimators, final_estimator=SVC).fit(X,y)
-test_y = clf.predict(test_X)
-predict = pd.DataFrame()
-predict['PassengerId'] = train_data['PassengerId'][891:]
+predict['PassengerId'] = all_data['PassengerId'][891:]
 predict['Survived'] = test_y.astype(int)
 predict.to_csv('predic.csv',index=False)
 """
-LR: 0.7957428214731583
-RC: 0.7957428214731584
-SGD: 0.7553058676654183
+LR: 0.7979900124843946
+RC: 0.7968539325842696
+SGD: 0.7384769038701624
 GNB: 0.7833832709113608
-KNN: 0.8193508114856428
-DTC: 0.7733707865168539
-LSVC: 0.7912609238451934
-SVC: 0.8305368289637952
-ABC: 0.8103745318352059
-BC: 0.8047191011235956
-GBC: 0.8339450686641697
-RFC: 0.8115230961298379
+KNN: 0.8170911360799001
+DTC: 0.7778027465667916
+LSVC: 0.7935081148564295
+SVC: 0.8294132334581773
+ABC: 0.8070037453183521
+BC: 0.813732833957553
+GBC: 0.8395505617977529
+RFC: 0.8036828963795255
 SC: 0.8283021223470662
-VC: 0.8283021223470662
+VC: 0.832796504369538
 """
